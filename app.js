4e1860993e27376f8c1fbe2cab029563b3336a47
Ramekici <46330945+Ramekici@users.noreply.github.com>
2020-12-29 03:28:47 +0300

    yer des
    ddd

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ const users = require('./routes/users');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use((req, res, next)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Methods",
+  "GET, POST, PATCH, DELETE, OPTIONS, PUT");
+  next();
+})
+
 dbConnect();
 
 app.use("/images", express.static(path.join("images")));
@@ -21,15 +30,6 @@ app.use("/images", express.static(path.join("images")));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev')); //istek gönderildiğinde bilgileri yazar
 
-
-app.use((req, res, next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.setHeader("Access-Control-Allow-Methods",
-  "GET, POST, PATCH, DELETE, OPTIONS, PUT");
-  next();
-})
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build')) 
