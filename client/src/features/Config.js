31ba39265da532d9443910734ac5ea757d5272ca
Ramekici <46330945+Ramekici@users.noreply.github.com>
2020-12-29 03:38:14 +0300

    ddd
    ddd

--- client/src/features/Config.js
+++ client/src/features/Config.js
@@ -1,3 +1,3 @@
-export const BACK_SERVER_URL = 'https://react-app-deneme.herokuapp.com'   //localhost
+export const BACK_SERVER_URL = 'https://nodemonapp.herokuapp.com'   //localhost
 
 //'https://react-app-deneme.herokuapp.com' 'http://localhost:5000'
